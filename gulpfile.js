d9dae439b5a25c45d2c0a84c1a32158f7459e11d
Martins Dumbris <martins.dumbris@gmail.com>
2016-06-27 13:12:54 +0300

    Set Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     mix.sass('app.scss')
+    	.browserify('app.js')
     	.browserSync({
     		proxy: 'resume.dev:8080'
     	});
